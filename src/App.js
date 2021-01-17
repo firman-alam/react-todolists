ed66bd2debc756056e685a028f90e5841d863749
hahahehe1 <51263803+hahahehe1@users.noreply.github.com>
2021-01-17 12:14:58 +0700

    mengatur header, todoform dan todos component

--- src/App.js
+++ src/App.js
@@ -2,49 +2,16 @@ import React from "react";
 import "./styles.css";
 
 import Paper from "./components/paper";
+import Header from "./components/header";
+import TodoForm from "./components/todoForm";
+import Todos from "./components/todos";
 
 export default function App() {
   return (
     <Paper>
-      <section className="header">
-        <button className="header-btn main-black-color">Add</button>
-        <h1 className="header-title">Todo Lists</h1>
-        <button className="header-btn main-red-color">Clear</button>
-      </section>
-
-      <section className="add">
-        <form className="add-form">
-          <input type="text" className="add-input" />
-          <button className="add-btn main-black-color">Add</button>
-        </form>
-      </section>
-
-      <section className="todos">
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-      </section>
+      <Header />
+      <TodoForm />
+      <Todos />
     </Paper>
   );
 }

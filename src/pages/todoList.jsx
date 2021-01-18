d2f04fbb2ddb15a6a09135f00d6dfe01f5ba22f8
hahahehe1 <51263803+hahahehe1@users.noreply.github.com>
2021-01-18 22:16:32 +0700

    conditional rendering

--- src/pages/todoList.jsx
+++ src/pages/todoList.jsx
@@ -12,16 +12,20 @@ const TodoList = () => {
     { text: "Learning Styling in React!" }
   ]);
 
+  const [showAdd, setShowAdd] = useState(false);
+
   const addTodo = (value) => {
     const addedTodo = [...todos, { text: value }];
 
     setTodos(addedTodo);
   };
 
+  const showAddToggle = () => setShowAdd(!showAdd);
+
   return (
     <Paper>
-      <Header />
-      <TodoForm addTodo={addTodo} />
+      <Header showAddToggle={showAddToggle} showAdd={showAdd} />
+      <TodoForm addTodo={addTodo} showAdd={showAdd} />
       <Todos todos={todos} />
     </Paper>
   );

5b20973ef92bc75f8860e7e07f119f37ec8e3955
hahahehe1 <51263803+hahahehe1@users.noreply.github.com>
2021-01-25 04:12:42 +0700

    emotion theming button dan header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,17 +2,19 @@
 //import React from "react";
 import { jsx } from "@emotion/react";
 import PropTypes from "prop-types";
-
+import { useTheme } from "@emotion/react";
 import Button from "../button/button";
 
 // import styles from "./header.module.css";
 import * as styles from "./header.styles";
 
 const Header = ({ showAddToggle, showAdd, clearTodos }) => {
+  const theme = useTheme();
+
   return (
     <section css={styles.header}>
       <Button text={showAdd ? "Finish" : "Add"} onClick={showAddToggle} />
-      <h1 css={styles.headerTitle}>Todo Lists</h1>
+      <h1 css={styles.headerTitle(theme)}>Todo Lists</h1>
       <Button text="Clear" onClick={clearTodos} color="red" align="right" />
     </section>
   );
--- src/components/header/header.styles.js
+++ src/components/header/header.styles.js
@@ -6,12 +6,12 @@ export const header = () =>
     align-items: flex-start;
   `;
 
-export const headerTitle = () =>
+export const headerTitle = (theme) =>
   css`
     width: 52%;
     text-align: center;
 
-    color: var(--main-black-color);
+    color: ${theme.color.primary.black};
     font-size: 3.8rem;
     font-family: "Sofia", sans-serif;
     text-transform: lowercase;

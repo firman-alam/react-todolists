8a142371ba17797e767575f31a163a4334ad4740
hahahehe1 <51263803+hahahehe1@users.noreply.github.com>
2021-01-30 23:27:27 +0700

    react router implementation (not finished)

---
+++ src/pages/about.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Paper from "../components/paper/paper";
+import Container from "../layout/container";
+
+const About = () => {
+  return (
+    <Paper>
+      <Container
+      // flexDirection="column"
+      // justifyContent="space-between"
+      // height="100%"
+      >
+        <h1>This is about page</h1>
+      </Container>
+    </Paper>
+  );
+};
